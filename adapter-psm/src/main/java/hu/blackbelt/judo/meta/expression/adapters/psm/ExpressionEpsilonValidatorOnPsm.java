7564e9e03c5387d7d208038fd215cc4c18f956e1
Robert Csakany <csakany.robert@gmail.com>
2024-11-04 01:03:26 +0100

    Fixing dependencies [ci skip]

--- adapter-psm/src/main/java/hu/blackbelt/judo/meta/expression/adapters/psm/ExpressionEpsilonValidatorOnPsm.java
+++ adapter-psm/src/main/java/hu/blackbelt/judo/meta/expression/adapters/psm/ExpressionEpsilonValidatorOnPsm.java
@@ -89,6 +89,8 @@ public class ExpressionEpsilonValidatorOnPsm extends ExpressionEpsilonValidator 
             // Transformation script
             executionContext
                     .executeProgram(evlExecutionContextBuilder().source(UriUtil.resolve("expression.evl", scriptRoot))
+                            // TODO: https://github.com/eclipse/epsilon/issues/133
+                            .parallel(false)
                             .expectedErrors(expectedErrors).expectedWarnings(expectedWarnings).build());
 
         } finally {
